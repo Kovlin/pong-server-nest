65f6a7edddb45b4d954bcb0438791ab81cf2febd
Kovlin <romainlink@hotmail.com>
2022-07-05 17:05:32 +0200

    interface

---
+++ src/interfaces/game.interface.ts
@@ -0,0 +1,14 @@
+export interface Game {
+	speed:  number;
+	speedU: number;
+	speedD: number;
+	interv: number;
+	padLen: number;
+	padHei: number;
+	ballR:  number;
+	ballX:  number;
+	ballY:  number;
+	ballYS: number;
+	ballXS: number;
+	score:  number;
+}
