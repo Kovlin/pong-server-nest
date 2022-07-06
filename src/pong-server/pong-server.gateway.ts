bf7b81950af632107fd4472c4eb5845b3f392e1a
Kovlin <romainlink@hotmail.com>
2022-07-06 17:26:19 +0200

    multiplayer

--- src/pong-server/pong-server.gateway.ts
+++ src/pong-server/pong-server.gateway.ts
@@ -1,25 +1,16 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import { WebSocketGateway, SubscribeMessage, MessageBody, ConnectedSocket } from '@nestjs/websockets';
 import { PongServerService } from './pong-server.service';
 import { CreatePongServerDto } from './dto/create-pong-server.dto';
 import { UpdatePongServerDto } from './dto/update-pong-server.dto';
 import { Logger } from '@nestjs/common';
-import { timingSafeEqual } from 'crypto';
-
-
-/** @WebSocketGateway The decorator can pass in some configuration options, such as the following example:
- *   @WebSocketGateway(80, {
- *     namespace: 'events',
- *     transports: ['websocket']
- *     cors: {
- *        origin: '*'
- *     },
- *     ...
- *   })
- **/
+import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway({ cors: true})
 export class PongServerGateway {
   constructor(private readonly pongServerService: PongServerService) {}
+
+	server: Server;
+	connUser : string[] = [];
 
   @SubscribeMessage('createPongServer')
   create(@MessageBody() createPongServerDto: CreatePongServerDto) {
@@ -46,36 +37,57 @@ export class PongServerGateway {
     return this.pongServerService.remove(id);
   }
 
-	@SubscribeMessage('socketTest')
-	socketTest(@MessageBody() data: any) {
-		Logger.log(data) // {test: 'test data'}
-		return {
-			msg1: 'Test 1',
-			msg2: 'Test 2',
-		}
-	}
-
 	@SubscribeMessage('speed')
-	speed(@MessageBody() data: any) {
+	speed(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
 		if (data[0] == 0)
-			this.pongServerService.stop();
+			this.pongServerService.stop(client, this.connUser);
 		else if (data[0] == 1)
-			this.pongServerService.up();
+			this.pongServerService.up(client, this.connUser);
 		else if (data[0] == 2)
-			this.pongServerService.down();
-		console.log(data[0]);
+			this.pongServerService.down(client, this.connUser);
+		// console.log(data[0]);
 	}
 
+	ready1: boolean;
+	ready2: boolean;
+	ready: boolean;
+	@SubscribeMessage('go')
+	go(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+
+		if (client.id == this.connUser[0])
+			this.ready1 = data[0];
+		if (client.id == this.connUser[1])
+			this.ready2 = data[0];
+		if (this.ready1 && this.ready2)
+			this.ready = true;
+		else
+			this.ready = false;
+		console.log(`ready: ${this.ready1} | ready1: ${this.ready1} | ready2: ${this.ready2}`);
+	}
+
+	player: number[];
 	@SubscribeMessage('start')
-	start(@MessageBody() data: any) {
-		this.pongServerService.logic(data[0], data[1]);
-		return {
-			ballX : this.pongServerService.ballX,
-			ballY : this.pongServerService.ballY,
-			x 		: this.pongServerService.x,
-			y 		: this.pongServerService.y,
-			score : this.pongServerService.score,
+	start(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+		if (this.ready)
+		{		
+			this.pongServerService.logic(data[0], data[1], this.connUser, client.id);
+			return {
+				ballX : this.pongServerService.ballX,
+				ballY : this.pongServerService.ballY,
+				x 		: this.pongServerService.x,
+				y 		: this.pongServerService.y,
+				x2 		: this.pongServerService.x2,
+				y2 		: this.pongServerService.y2,
+				score : this.pongServerService.score,
+				score2 : this.pongServerService.score2,
+			}
 		}
 	}
 
+	
+	handleConnection(socket: Socket) {
+		console.log(socket.id);
+		this.connUser.push(socket.id);
+		// console.log(socket);
+	}
 }
--- src/pong-server/pong-server.service.ts
+++ src/pong-server/pong-server.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
 import { CreatePongServerDto } from './dto/create-pong-server.dto';
 import { UpdatePongServerDto } from './dto/update-pong-server.dto';
 
@@ -28,6 +29,7 @@ export class PongServerService {
 	//Game Logic :
 
 	speed = 0;
+	speed2 = 0;
 	speedU = -10;
 	speedD = 10;
 	interv: any;
@@ -35,64 +37,116 @@ export class PongServerService {
 	padHei: number = 15;
 
 	ballR = 10;
-	ballX = 120;
-	ballY = 120;
-	ballYS = -5;
-	ballXS = 5;
-	score = 0;	
+	ballYS = 5;
+	ballXS = -5; 
+	score = 0;
+	score2 = 0;
+	canW = 650;
+	canH = 480;
+	ballX = this.canW / 2;
+	ballY = this.canH / 2;
+	ballSpeed = 5;
 
-	x = 5;
-	y = Math.floor(473 / 8);
-
-	up(): void {
-		this.speed = this.speedU;
+	startDirection() {
+		let tmp = Math.random() * 1 + 1;
+		if (tmp == 1)
+			this.ballXS = -this.ballSpeed;
+		else
+			this.ballXS = -this.ballSpeed;
+		let tmp2 = Math.random() * 5 + 1;
+			this.ballYS = tmp2;
+		this.ballX = this.canW / 2;
+		this.ballY = this.canH / 2;
 	}
 
-	down(): void {
-		this.speed = this.speedD;
+	x = 0;
+	y = Math.floor(this.canH / 8);
+
+	x2 = this.canW - this.padHei;
+	y2 = Math.floor(this.canH / 8);
+
+	up(client: Socket, players: string[]): void {
+		if (client.id == players[0])
+			this.speed = this.speedU;
+		else if (client.id == players[1])
+			this.speed2 = this.speedU;
 	}
 
-	stop(): void {
-		this.speed = 0;
+	down(client: Socket, players: string[]): void {
+		if (client.id == players[0])
+			this.speed = this.speedD;
+		else if (client.id == players[1])
+			this.speed2 = this.speedD;
 	}
 
-	inBound(canH: number): boolean {
-		let check =  canH;
-		if ((this.y + this.speed < 0) || (this.y + this.padLen + this.speed >  canH))
-			return false;
+	stop(client: Socket, players: string[]): void {
+		if (client.id == players[0])
+			this.speed = 0;
+		else if (client.id == players[1])
+			this.speed2 = 0;
+	}
+
+	inBound(canH: number, player: number): boolean {
+		if (player == 0){
+			if ((this.y + this.speed < 0) || (this.y + this.padLen + this.speed >  canH))
+				return false;
+		}
+		if (player == 1){
+			if ((this.y2 + this.speed2 < 0) || (this.y2 + this.padLen + this.speed2 >  canH))
+				return false;
+		}
 		return true;
 	}
 
-	logic(canW:number, canH: number): void {
-		if (this.inBound(canH))
+	leftCheck() {
+		if (this.ballX < this.padHei + this.x) {
+			if (this.ballY >= this.y && this.ballY <= this.y + this.padLen) {
+				this.ballXS = -this.ballXS;
+			}
+		}
+	}
+
+	rightCheck() {
+		if (this.ballX + this.ballR > this.x2) {
+			if (this.ballY >= this.y2 && this.ballY <= this.y2 + this.padLen) {
+				this.ballXS = -this.ballXS;
+			}
+		}
+	}
+
+	launchBall() {
+		this.startDirection();
+	}
+
+	logic(canW:number, canH: number, player: string[], client: string): void {
+		if (this.inBound(canH, 0) && player[0] == client)
 			this.y += this.speed;
+		else if (this.inBound(canH, 1) && player[1] == client)
+			this.y2 += this.speed2;
 
 		if (this.ballY < this.ballR || this.ballY > canH - this.ballR) {
 			this.ballYS = - this.ballYS;
 		}
 
-		if (this.ballX < this.ballR) {
-			this.ballX = 120;
-			this.ballY = 120;
-			this.ballYS = -5;
-			this.ballXS = 5;
-			this.score = 0;
+		if (this.ballX < this.ballR)
+		{
+			this.score2 += 1;
+			this.launchBall();
 		}
-
-		if (this.ballX > canW) {
-			this.ballX -= this.ballXS;
-			this.ballXS = -this.ballXS
+		else if (this.ballX > this.canW - this.ballR)
+		{
+			this.score += 1;
+			this.launchBall();
 		}
 
 		this.ballY += this.ballYS;
 		this.ballX += this.ballXS;
 
-		if (this.ballX - this.ballR <= this.padHei + this.x && this.ballX > this.x) {
-			if (this.ballY >= this.y && this.ballY <= this.y + this.padLen) {
-				this.ballXS = -this.ballXS;
-				this.score += 1;
-			}
-		}
+		if (this.ballX < canW / 2)
+			this.leftCheck();
+		else
+			this.rightCheck();
+
 	}
 
 }
